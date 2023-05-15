fcf71a63ce7b7c523a08454ec40539e70c2365bf
SzegBer <szegedi.bernadett@gmail.com>
2023-05-15 19:59:51 +0200

    parameter option filters set up

--- src/components/Parameters.jsx
+++ src/components/Parameters.jsx
@@ -1,68 +1,159 @@
 // you can chose the parameters for the recepie you are searching for
 
-import { useCallback, useEffect, useState } from "react";
+import { useState, useEffect, useCallback } from "react";
 import Recepie from "./Recepie";
 
 
-const Parameters = () => {
 
-  let options = {
-    cuisine: 'italian',
-    diet: '',
+const Parameters = () => {
+  
+  const [ showCuisineOptions, setShowCuisineOptions ] = useState(false)
+  const [ showTypeOptions, setShowTypeOptions ] = useState(false)
+  const [ showDietOptions, setShowDietOptions ] = useState(false)
+  const [ options, setOptions ] = useState({
+    cuisine: '',
+    diet: null,
     type: '',
     maxReadyTime: '60',
     nr: '6'
+  })
+
+  // hides / shows the sub-options of the selected option
+  const displayOptions = (e) => {
+
+    switch(e.target.value){
+      case 'cuisine':
+        showCuisineOptions ? setShowCuisineOptions(false) : setShowCuisineOptions(true);
+        break
+      
+      case 'type':
+        showTypeOptions ? setShowTypeOptions(false) : setShowTypeOptions(true);
+        break
+
+      case 'diet':
+        showDietOptions ? setShowDietOptions(false) : setShowDietOptions(true);
+        break
+      
+      default:
+        console.log('switch default case, Error in target value!')
+    }    
   }
 
+  // changes options state cuisine property value (single value)
+  const changeCuisine = (e) => {
+    setOptions({
+      ...options,
+      cuisine: e.target.value
+    })
+  }
+  
+  // changes options state type property value (single value)
+  const changeType = (e) => {
+    setOptions({
+      ...options,
+      type: e.target.value
+    })
+  }
 
+  // changes options state diet property value (multi values)
+  const changeDiet = (e) => {
+    if(options.diet === null){
+      setOptions({
+        ...options,
+        diet: e.target.name
+      })
+    }
+    else if (options.diet.includes(e.target.name)) {
+      setOptions({
+        ...options,
+        diet: options.diet.replace(`,${e.target.name}`, "")
+      })
+    }
+    else {
+      setOptions({
+        ...options,
+        diet: options.diet+","+e.target.name
+      })
+    }  
+  }
+
+  // building the url for the fetch based on the option states
+  let url = ""
+  const urlBuilder = () => {
+    const baseUrl = 'https://api.spoonacular.com/recipes/complexSearch'
+    const apiKey = 'f9d7f1bf8069414388b168f2bd13947b'
+
+    const cuisine = options.cuisine
+    const diet = options.diet
+    const type = options.type
+    const maxReadyTime = options.maxReadyTime
+    const nr = options.nr
+  
+    url = `${baseUrl}?apiKey=${apiKey}&cuisine=${cuisine}&diet=${diet}&type=${type}&maxReadyTime=${maxReadyTime}&number=${nr}`
+  }
+  urlBuilder()
 
   return (
     <div className="parameters">
 
-      <button>search by cuisine</button>
+      <button value="cuisine" onClick={displayOptions}>search by cuisine</button>
+      {
+        showCuisineOptions && 
         <div className="filter cuisine">
-          <button>African</button>
-          <button>American</button>
-          <button>Caribbean</button>
-          <button>Chinese</button>
-          <button>French</button>
-          <button>Greek</button>
-          <button>Indian</button>
-          <button>Japanese</button>
-          <button>Mexican</button>
-          <button>Thai</button>
+          <button value="African" onClick={changeCuisine}>African</button>
+          <button value="American" onClick={changeCuisine}>American</button>
+          <button value="Caribbean" onClick={changeCuisine}>Caribbean</button>
+          <button value="Chinese" onClick={changeCuisine}>Chinese</button>
+          <button value="French" onClick={changeCuisine}>French</button>
+          <button value="Greek" onClick={changeCuisine}>Greek</button>
+          <button value="Indian" onClick={changeCuisine}>Indian</button>
+          <button value="Italian" onClick={changeCuisine}>Italian</button>
+          <button value="Japanese" onClick={changeCuisine}>Japanese</button>
+          <button value="Mexican" onClick={changeCuisine}>Mexican</button>
+          <button value="Thai" onClick={changeCuisine}>Thai</button>
         </div>
+      }       
 
-      {/*<button>search by type</button>
+      <button value="type" onClick={displayOptions}>search by type</button>
+      {
+        showTypeOptions &&
         <div className="filter type">
-          <button>breakfast</button>
-          <button>main course</button>
-          <button>dessert ... etc</button>
+          <button value="main%20course" onClick={changeType}>main course</button>
+          <button value="side%20dish" onClick={changeType}>side dish</button>
+          <button value="dessert" onClick={changeType}>dessert</button>
+          <button value="appetizer" onClick={changeType}>appetizer</button>
+          <button value="breakfast" onClick={changeType}>breakfast</button>
+          <button value="soup" onClick={changeType}>soup</button>
+          <button value="snack" onClick={changeType}>snack</button>
+          <button value="drink" onClick={changeType}>drink</button>
         </div>
+      }
 
-      <button>search by diet</button>
-      <div className="filter diet">
-        <label>
-          <span>Vegetarian</span>
-          <input type="checkbox" name="vegetarian"></input>
-        </label>
-        <label>
-          <span>Vegan</span>
-          <input type="checkbox" name="vegan"></input>
-        </label>
-        <label>
-          <span>Dairy Free</span>
-          <input type="checkbox" name="dairyFree"></input>
-        </label>
-        <label>
-          <span>Gluten Free</span>
-          <input type="checkbox" name="glutenFree"></input>
-        </label>
-      </div>*/}
+      <button value="diet" onClick={displayOptions}>search by diet</button>
+      {
+        showDietOptions &&
+        <div className="filter diet">
+          <label>
+            <span>Vegetarian</span>
+            <input type="checkbox" name="vegetarian" onClick={changeDiet}></input>
+          </label>
+          <label>
+            <span>Vegan</span>
+            <input type="checkbox" name="vegan" onClick={changeDiet}></input>
+          </label>
+          <label>
+            <span>Dairy Free</span>
+            <input type="checkbox" name="dairyFree" onClick={changeDiet}></input>
+          </label>
+          <label>
+            <span>Gluten Free</span>
+            <input type="checkbox" name="glutenFree" onClick={changeDiet}></input>
+          </label>
+        </div>
+      }
 
-      <button>START THE SEARCH</button>
-      <Recepie options={options}/>
-
+      <Recepie filters={url} />
+      
     </div>
   )
 
--- src/components/Recepie.jsx
+++ src/components/Recepie.jsx
@@ -4,48 +4,33 @@ import { useFetch } from '../hooks/useFetch'
 import './styles/Recepie.css'
 
 
-const Recepie  = (props) => {
+const Recepie = (props) => {
 
   // receive options
   // assemble the right url for the fetch
   // fetch and give the data to map function to display a few of them.
 
-  let options = props.options
-  console.log(options)
+  const url = props.filters
+  console.log("feccs",url)
+  //console.log(options)
 
-  let baseUrl = 'https://api.spoonacular.com/recipes/complexSearch'
-  let apiKey = 'f9d7f1bf8069414388b168f2bd13947b'
-
-  let cuisine = 'italian'
-  let diet = ''
-  let type = ''
-  let maxReadyTime = '60'
-  let nr = '6'
-
-  let url = `${baseUrl}?apiKey=${apiKey}&cuisine=${cuisine}&diet=${diet}&type=${type}&maxReadyTime=${maxReadyTime}&number=${nr}`
-
-  /*let { data } = useFetch(url)
-  console.log(data)*/
+  const { data: recepies } = useFetch(url)
 
   return (
-    <div className="hits">
+    <div className="recepies">
+      <h2>RANDOM RECEPIES</h2>
 
-      <div className="recepie">  {/*MAPPELNI*/}
-        <img className="picture" src="https://spoonacular.com/recipeImages/716429-556x370.jpg" alt="Pasta" />
-        <h2 className="title">title: Pasta with Garlic, Scallions, Cauliflower & Breadcrumbs</h2>
-        <p className="ready">readyInMinutes: 45</p>
-        <p className="source">sourceName: Full Belly Sisters</p>
-        <p className="cuisines">cuisines: []</p>
-        <div className="diets">
-          <p>glutenFree: false</p>
-          <p>dairyFree: false</p>
-          <p>vegan: false</p>
-          <p>vegetarian: false</p>
-        </div>
-      </div> 
-
+      <div className="hits">
+        {
+        recepies && recepies.results.map((recepie) => (
+          <div className="recepie" key={recepie.id}>
+            <img className="picture" src={recepie.image} alt={recepie.title} />
+            <h3 className="title">{recepie.title}</h3>
+          </div> 
+        ))
+        }
+      </div>
     </div>
-    
   )
 }
 
