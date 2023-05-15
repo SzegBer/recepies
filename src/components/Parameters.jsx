240b57c7abcda623e8d5d3deefcbe475cda6d16f
SzegBer <szegedi.bernadett@gmail.com>
2023-05-15 22:00:45 +0200

    conditional formatting for chosed parameters

--- src/components/Parameters.jsx
+++ src/components/Parameters.jsx
@@ -1,15 +1,16 @@
 // you can chose the parameters for the recepie you are searching for
 
-import { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import Recepie from "./Recepie";
-
-
+import './styles/Parameters.css'
 
 const Parameters = () => {
   
   const [ showCuisineOptions, setShowCuisineOptions ] = useState(false)
   const [ showTypeOptions, setShowTypeOptions ] = useState(false)
   const [ showDietOptions, setShowDietOptions ] = useState(false)
+  //const chosedAsParameter = false
+
   const [ options, setOptions ] = useState({
     cuisine: '',
     diet: null,
@@ -39,120 +40,119 @@ const Parameters = () => {
     }    
   }
 
-  // changes options state cuisine property value (single value)
+  // changes options state _cuisine property_ value (single value)
   const changeCuisine = (e) => {
+
     setOptions({
       ...options,
       cuisine: e.target.value
     })
+    let els = document.querySelector('.cuisine').getElementsByClassName('chosed')
+    if(els){
+      while (els[0]) {
+        els[0].classList.remove('chosed')
+      }
+    }
+    e.target.className = 'chosed'
   }
   
-  // changes options state type property value (single value)
+  // changes options state _type property_ value (single value)
   const changeType = (e) => {
     setOptions({
       ...options,
       type: e.target.value
     })
+    let els = document.querySelector('.type').getElementsByClassName('chosed')
+    if(els){
+      while (els[0]) {
+        els[0].classList.remove('chosed')
+      }
+    }
+    e.target.className = 'chosed'
   }
 
-  // changes options state diet property value (multi values)
+
+  // updates options state _diet property_ value (multi values) + toggles classNames
   const changeDiet = (e) => {
     if(options.diet === null){
       setOptions({
         ...options,
-        diet: e.target.name
+        diet: e.target.value
       })
     }
-    else if (options.diet.includes(e.target.name)) {
+    else if (options.diet.includes(e.target.value)) {
       setOptions({
         ...options,
-        diet: options.diet.replace(`,${e.target.name}`, "")
+        diet: options.diet.replace(`,${e.target.value}`, "")
       })
     }
     else {
       setOptions({
         ...options,
-        diet: options.diet+","+e.target.name
+        diet: options.diet+","+e.target.value
       })
-    }  
+    }
+
+    if(e.target.className === ""){
+      e.target.className = "chosed"
+    } else {
+      e.target.className = ""
+    }
   }
 
-  // building the url for the fetch based on the option states
-  let url = ""
-  const urlBuilder = () => {
-    const baseUrl = 'https://api.spoonacular.com/recipes/complexSearch'
-    const apiKey = 'f9d7f1bf8069414388b168f2bd13947b'
+  const cuisines = ["African", "American", "Caribbean", "Chinese", "French", "Greek", "Indian", "Italian", "Japanese", "Mexican", "Thai"]
 
-    const cuisine = options.cuisine
-    const diet = options.diet
-    const type = options.type
-    const maxReadyTime = options.maxReadyTime
-    const nr = options.nr
-  
-    url = `${baseUrl}?apiKey=${apiKey}&cuisine=${cuisine}&diet=${diet}&type=${type}&maxReadyTime=${maxReadyTime}&number=${nr}`
-  }
-  urlBuilder()
+  const types = [
+    { name: 'main course', value: 'main%20course'},
+    { name: 'side dish', value: 'side%20dish'},
+    { name: 'dessert', value: 'dessert'},
+    { name: 'appetizer', value: 'appetizer'},
+    { name: 'breakfast', value: 'breakfast'},
+    { name: 'soup', value: 'soup'},
+    { name: 'snack', value: 'snack'},
+    { name: 'drink', value: 'drink'}
+  ]
+
+  const diets = [
+    { name: 'vegetarian', value: 'vegetarian'},
+    { name: 'vegan', value: 'vegan'},
+    { name: 'dairy free', value: 'dairyFree'},
+    { name: 'gluten free', value: 'glutenFree'}
+  ]
+
 
   return (
     <div className="parameters">
 
-      <button value="cuisine" onClick={displayOptions}>search by cuisine</button>
-      {
-        showCuisineOptions && 
-        <div className="filter cuisine">
-          <button value="African" onClick={changeCuisine}>African</button>
-          <button value="American" onClick={changeCuisine}>American</button>
-          <button value="Caribbean" onClick={changeCuisine}>Caribbean</button>
-          <button value="Chinese" onClick={changeCuisine}>Chinese</button>
-          <button value="French" onClick={changeCuisine}>French</button>
-          <button value="Greek" onClick={changeCuisine}>Greek</button>
-          <button value="Indian" onClick={changeCuisine}>Indian</button>
-          <button value="Italian" onClick={changeCuisine}>Italian</button>
-          <button value="Japanese" onClick={changeCuisine}>Japanese</button>
-          <button value="Mexican" onClick={changeCuisine}>Mexican</button>
-          <button value="Thai" onClick={changeCuisine}>Thai</button>
-        </div>
-      }       
+      <button className="options" value="cuisine" onClick={displayOptions}>search by cuisine</button>
+      <div className="filter cuisine">
+        {showCuisineOptions && cuisines.map((cuisine, i) => (
+          <React.Fragment key={cuisine+i}>
+            <button value={cuisine} onClick={changeCuisine}>{cuisine}</button>
+          </React.Fragment>
+        ))}
+      </div>    
 
-      <button value="type" onClick={displayOptions}>search by type</button>
-      {
-        showTypeOptions &&
-        <div className="filter type">
-          <button value="main%20course" onClick={changeType}>main course</button>
-          <button value="side%20dish" onClick={changeType}>side dish</button>
-          <button value="dessert" onClick={changeType}>dessert</button>
-          <button value="appetizer" onClick={changeType}>appetizer</button>
-          <button value="breakfast" onClick={changeType}>breakfast</button>
-          <button value="soup" onClick={changeType}>soup</button>
-          <button value="snack" onClick={changeType}>snack</button>
-          <button value="drink" onClick={changeType}>drink</button>
-        </div>
-      }
+      <button className="options" value="type" onClick={displayOptions}>search by type</button>
+      <div className="filter type">
+        {showTypeOptions && types.map((type, i) => (
+          <React.Fragment key={type.name+i}>
+            <button value={type.value} onClick={changeType}>{type.name}</button>
+          </React.Fragment>
+        ))}
+      </div>
+      
 
-      <button value="diet" onClick={displayOptions}>search by diet</button>
-      {
-        showDietOptions &&
+      <button className="options" value="diet" onClick={displayOptions}>search by diet</button>
         <div className="filter diet">
-          <label>
-            <span>Vegetarian</span>
-            <input type="checkbox" name="vegetarian" onClick={changeDiet}></input>
-          </label>
-          <label>
-            <span>Vegan</span>
-            <input type="checkbox" name="vegan" onClick={changeDiet}></input>
-          </label>
-          <label>
-            <span>Dairy Free</span>
-            <input type="checkbox" name="dairyFree" onClick={changeDiet}></input>
-          </label>
-          <label>
-            <span>Gluten Free</span>
-            <input type="checkbox" name="glutenFree" onClick={changeDiet}></input>
-          </label>
+          {showDietOptions && diets.map((diet,i) => (
+            <React.Fragment key={diet.name+i}>
+              <button value={diet.value} onClick={changeDiet}>{diet.name}</button>
+            </React.Fragment>
+          ))}
         </div>
-      }
 
-      <Recepie filters={url} />
+      {/*<Recepie options={options} />*/}
       
     </div>
   )
--- src/components/Recepie.jsx
+++ src/components/Recepie.jsx
@@ -10,10 +10,24 @@ const Recepie = (props) => {
   // assemble the right url for the fetch
   // fetch and give the data to map function to display a few of them.
 
-  const url = props.filters
+  const options = props.options
+
+  const urlBuilder = () => {
+    const baseUrl = 'https://api.spoonacular.com/recipes/complexSearch'
+    const apiKey = 'f9d7f1bf8069414388b168f2bd13947b'
+
+    const cuisine = options.cuisine
+    const diet = options.diet
+    const type = options.type
+    const maxReadyTime = options.maxReadyTime
+    const nr = options.nr
+  
+    return `${baseUrl}?apiKey=${apiKey}&cuisine=${cuisine}&diet=${diet}&type=${type}&maxReadyTime=${maxReadyTime}&number=${nr}`
+  }
+
   console.log("feccs",url)
   //console.log(options)
-
+  let url = urlBuilder()
   const { data: recepies } = useFetch(url)
 
   return (
